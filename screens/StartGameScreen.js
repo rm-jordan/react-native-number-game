433d1f13b8d31acecc9bd9f1027dd60a7ee6d951
rm-jordan <64985248+rm-jordan@users.noreply.github.com>
2022-01-18 08:21:38 -0500

    created Card.js, exported styles

--- screens/StartGameScreen.js
+++ screens/StartGameScreen.js
@@ -2,18 +2,22 @@ import React from "react";
 
 import { View, Text, StyleSheet, TextInput, Button } from "react-native";
 
+import Card from "../components/Card";
+
 const StartGameScreen = (props) => {
   return (
     <View style={styles.screen}>
       <Text style={styles.title}>Start a New Game!!!</Text>
-      <View style={styles.inputContainer}>
-        <Text>Select a number</Text>
-        <TextInput />
-        <View style={styles.buttonContainer}>
-          <Button title="Reset" onPress={() => {}} />
-          <Button title="Confirm" onPress={() => {}} />
+      <Card style={styles.inputContainer}>
+        <View style={styles.inputContainer}>
+          <Text>Select a number</Text>
+          <TextInput />
+          <View style={styles.buttonContainer}>
+            <Button title="Reset" onPress={() => {}} />
+            <Button title="Confirm" onPress={() => {}} />
+          </View>
         </View>
-      </View>
+      </Card>
     </View>
   );
 };
@@ -32,15 +36,6 @@ const styles = StyleSheet.create({
     width: 300,
     maxWidth: "80%",
     alignItems: "center",
-    shadowColor: "black",
-    shadowOffset: { width: 0, height: 2 },
-    shadowRadius: 6,
-    shadowOpacity: 0.26,
-    // elevation for Android
-    elevation: 5,
-    backgroundColor: "white",
-    padding: 20,
-    borderRadius: 10,
   },
   buttonContainer: {
     flexDirection: "row",
