6a75f87c2cee20507dd949c4eaa7bd7e3152aaad
rm-jordan <64985248+rm-jordan@users.noreply.github.com>
2022-01-18 07:31:33 -0500

    styled StartGameScreen, added TextInput

--- screens/StartGameScreen.js
+++ screens/StartGameScreen.js
@@ -5,11 +5,11 @@ import { View, Text, StyleSheet, TextInput, Button } from "react-native";
 const StartGameScreen = (props) => {
   return (
     <View style={styles.screen}>
-      <Text>Start a New Game!!!</Text>
-      <View>
+      <Text style={styles.title}>Start a New Game!!!</Text>
+      <View style={styles.inputContainer}>
         <Text>Select a number</Text>
         <TextInput />
-        <View>
+        <View style={styles.buttonContainer}>
           <Button title="Reset" onPress={() => {}} />
           <Button title="Confirm" onPress={() => {}} />
         </View>
@@ -23,6 +23,21 @@ const styles = StyleSheet.create({
     flex: 1,
     padding: 10,
     alignItems: "center",
+  },
+  title: {
+    fontSize: 20,
+    marginVertical: 10,
+  },
+  inputContainer: {
+    width: 300,
+    maxWidth: "80%",
+    alignItems: "center",
+  },
+  buttonContainer: {
+    flexDirection: "row",
+    width: "100%",
+    justifyContent: "space-between",
+    paddingHorizontal: 15,
   },
 });
 
