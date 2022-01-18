1de9a463b05da52cd471d98b74ea5b9bc192018c
rm-jordan <64985248+rm-jordan@users.noreply.github.com>
2022-01-18 06:59:16 -0500

    created header component and styled

--- App.js
+++ App.js
@@ -1,8 +1,17 @@
-import { StatusBar } from "expo-status-bar";
 import { StyleSheet, View } from "react-native";
+import Header from "./components/Header";
 
 export default function App() {
-  return <View></View>;
+  return (
+    <View style={styles.screen}>
+      <Header title="Guess a number!" />
+    </View>
+  );
 }
 
-const styles = StyleSheet.create({});
+// flex 1 ensure the view takes all the space it can get
+const styles = StyleSheet.create({
+  screen: {
+    flex: 1,
+  },
+});
--- components/Header.js
+++ components/Header.js
@@ -11,8 +11,18 @@ const Header = (props) => {
 };
 
 const styles = StyleSheet.create({
-  header: {},
-  headerTitle: {},
+  header: {
+    width: "100%",
+    height: 90,
+    paddingTop: 36,
+    backgroundColor: "#f7287b",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  headerTitle: {
+    color: "black",
+    fontSize: 18,
+  },
 });
 
 export default Header;
