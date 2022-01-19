737e70b8513f01d8dc1e7c8dd10c5e8b863e7ae8
rm-jordan <64985248+rm-jordan@users.noreply.github.com>
2022-01-19 08:48:08 -0500

    styled input and started TextInput configuration

--- screens/StartGameScreen.js
+++ screens/StartGameScreen.js
@@ -1,8 +1,9 @@
 import React from "react";
 
-import { View, Text, StyleSheet, TextInput, Button } from "react-native";
+import { View, Text, StyleSheet, Button } from "react-native";
 import Card from "../components/Card";
 import colours from "../constants/colours";
+import Input from "../components/Input";
 
 const StartGameScreen = (props) => {
   return (
@@ -11,7 +12,7 @@ const StartGameScreen = (props) => {
       <Card style={styles.inputContainer}>
         <View style={styles.inputContainer}>
           <Text>Select a number</Text>
-          <TextInput />
+          <Input style={styles.input} />
           <View style={styles.buttonContainer}>
             <View style={styles.button}>
               <Button title="Reset" onPress={() => {}} color={colours.accent} />
@@ -53,6 +54,10 @@ const styles = StyleSheet.create({
   },
   button: {
     width: 90,
+  },
+  input: {
+    width: 50,
+    textAlign: "center",
   },
 });
 
