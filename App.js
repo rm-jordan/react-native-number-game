c35cef4ff72ebd867f3b160220a2cc276fbed038
rm-jordan <64985248+rm-jordan@users.noreply.github.com>
2022-01-20 10:27:22 -0500

    added buttons, added to game logic, passed down lprops to gamescreen

--- App.js
+++ App.js
@@ -1,12 +1,28 @@
+import React, { useState } from "react";
 import { StyleSheet, View } from "react-native";
 import Header from "./components/Header";
 import StartGameScreen from "./screens/StartGameScreen";
+import GameScreen from "./screens/GameScreen";
 
 export default function App() {
+  const [userNumber, setUserNumber] = useState();
+
+  const startGameHandler = (selectedNumber) => {
+    setUserNumber(selectedNumber);
+  };
+
+  let content = <StartGameScreen onStartGame={startGameHandler} />;
+
+  if (userNumber) {
+    content = <GameScreen userChoice={userNumber} />;
+  }
+
+  // render GameScreen conditionally when button is selected
+
   return (
     <View style={styles.screen}>
       <Header title="Guess a number!" />
-      <StartGameScreen />
+      {content}
     </View>
   );
 }
--- screens/GameScreen.js
+++ screens/GameScreen.js
@@ -1,15 +1,52 @@
-import React from "react";
+import React, { useState } from "react";
 
-import { View, Text, StyleSheet } from "react-native";
+import { View, Text, StyleSheet, Button } from "react-native";
+import NumberContainer from "../components/NumberContainer";
+import Card from "../components/Card";
 
 const generateRandomBetween = (min, max, exclude) => {
   min = Math.ceil(min);
   max = Math.floor(max);
   const rndNum = Math.floor(Math.random() * (max - min)) + min;
+  if (rndNum === exclude) {
+    return generateRandomBetween(min, max, exclude);
+  } else {
+    return rndNum;
+  }
 };
 
-const GameScreen = (props) => {};
+const GameScreen = (props) => {
+  // generate random number but wont override state when called again
+  // only considered if no initial state set
+  const [currentGuess, setCurrentGuess] = useState(
+    generateRandomBetween(1, 100, props.userChoice)
+  );
+  return (
+    <View styles={styles.screen}>
+      <Text>Opponent's Guess</Text>
+      <NumberContainer>{currentGuess}</NumberContainer>
+      <Card style={styles.buttonContainer}>
+        <Button title="LOWER" onPress={() => {}} />
+        <Button title="HIGHER" onPress={() => {}} />
+      </Card>
+    </View>
+  );
+};
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  screen: {
+    flex: 1,
+    padding: 10,
+    alignItems: "center",
+  },
+  buttonContainer: {
+    flexDirection: "row",
+    alignItems: "center",
+    justifyContent: "space-around",
+    marginTop: 20,
+    width: 300,
+    maxWidth: "80%",
+  },
+});
 
 export default GameScreen;
