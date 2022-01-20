33b572f329e3e173c292a5748e4eb02f503b0604
rm-jordan <64985248+rm-jordan@users.noreply.github.com>
2022-01-20 09:25:38 -0500

    created number generator and logic

--- screens/GameScreen.js
+++ screens/GameScreen.js
@@ -2,6 +2,12 @@ import React from "react";
 
 import { View, Text, StyleSheet } from "react-native";
 
+const generateRandomBetween = (min, max, exclude) => {
+  min = Math.ceil(min);
+  max = Math.floor(max);
+  const rndNum = Math.floor(Math.random() * (max - min)) + min;
+};
+
 const GameScreen = (props) => {};
 
 const styles = StyleSheet.create({});
