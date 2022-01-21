c4cbd8707684a3d7e2b02067942c8dcbdbf9d39c
rm-jordan <64985248+rm-jordan@users.noreply.github.com>
2022-01-21 08:43:56 -0500

    logic to direct gameover screen adjusted

--- App.js
+++ App.js
@@ -3,18 +3,31 @@ import { StyleSheet, View } from "react-native";
 import Header from "./components/Header";
 import StartGameScreen from "./screens/StartGameScreen";
 import GameScreen from "./screens/GameScreen";
+import GameOver from "./screens/GameOver";
 
 export default function App() {
   const [userNumber, setUserNumber] = useState();
 
+  //need to set up logic for # of rounds
+  const [guessRounds, setGuessRounds] = useState(0);
+
   const startGameHandler = (selectedNumber) => {
     setUserNumber(selectedNumber);
+    setGuessRounds(0); // at start of new game rounds =0
+  };
+
+  const gameOverHandler = (numOfRounds) => {
+    setGuessRounds(numOfRounds);
   };
 
   let content = <StartGameScreen onStartGame={startGameHandler} />;
 
-  if (userNumber) {
-    content = <GameScreen userChoice={userNumber} />;
+  if (userNumber && guessRounds <= 0) {
+    content = (
+      <GameScreen userChoice={userNumber} onGameOver={gameOverHandler} />
+    );
+  } else if (guessRounds > 0) {
+    content = <GameOver />;
   }
 
   // render GameScreen conditionally when button is selected
--- screens/GameScreen.js
+++ screens/GameScreen.js
@@ -22,14 +22,20 @@ const GameScreen = (props) => {
     generateRandomBetween(1, 100, props.userChoice)
   );
 
+  const [rounds, setRounds] = useState(0);
+
   const currentLow = useRef(1);
   const currentHigh = useRef(100);
+
+  const { userChoice, onGameOver } = props;
 
   //runs after every render cycle
   useEffect(() => {
     if (currentGuess === props.userChoice) {
+      props.onGameOver(rounds);
     }
-  });
+    // destructured because using props would change whenever parent changes
+  }, [currentGuess, userChoice, onGameOver]);
 
   const nextGuessHandler = (direction) => {
     if (
@@ -53,6 +59,7 @@ const GameScreen = (props) => {
       currentGuess
     );
     setCurrentGuess(nextNumber);
+    setRounds((curRounds) => curRounds + 1);
   };
 
   return (
