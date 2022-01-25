c7324d82f3806b8941f4bb4be118ec90055c04fd
rm-jordan <64985248+rm-jordan@users.noreply.github.com>
2022-01-25 14:03:39 -0500

    troubleshooting font import issues, some styling adjustments

--- App.js
+++ App.js
@@ -1,15 +1,38 @@
 import React, { useState } from "react";
 import { StyleSheet, View } from "react-native";
+
 import Header from "./components/Header";
 import StartGameScreen from "./screens/StartGameScreen";
 import GameScreen from "./screens/GameScreen";
 import GameOver from "./screens/GameOver";
+
+import * as Font from "expo-font";
+import { AppLoading } from "expo";
+
+const fetchFonts = () => {
+  return Font.loadAsync({
+    "open-sans": require("./assets/fonts/OpenSans-Regular.ttf"),
+    "open-sans-bold": require("./assets/fonts/OpenSans-Bold.ttf"),
+  });
+};
 
 export default function App() {
   const [userNumber, setUserNumber] = useState();
 
   //need to set up logic for # of rounds
   const [guessRounds, setGuessRounds] = useState(0);
+
+  const [dataLoaded, setDataLoaded] = useState(false);
+
+  if (!dataLoaded) {
+    return (
+      <AppLoading
+        startAsync={fetchFonts}
+        onFinish={() => setDataLoaded(true)}
+        onError={(error) => console.log(error)}
+      />
+    );
+  }
 
   const configureNewGameHandler = () => {
     setGuessRounds(0);
